b5b26d9c9468602e5edcafd0a4675f6cfd8fcee7
_ybv <vaibhavk@buffalo.edu>
2013-09-10 22:48:03 -0400

    changes in file names

--- Flasktest/views.py
+++ Flasktest/views.py
@@ -26,30 +26,6 @@ twitter = oauth.remote_app('twitter',
     consumer_secret='3Htb9jsBnVdBrt1iymC2ABKeaJfljcn4L06L5hlxKs'
 )
 
-
-
-twitter = oauth.remote_app('twitter',
-    # unless absolute urls are used to make requests, this will be added
-    # before all URLs.  This is also true for request_token_url and others.
-    base_url='https://api.twitter.com/1/',
-    # where flask should look for new request tokens
-    request_token_url='https://api.twitter.com/oauth/request_token',
-    # where flask should exchange the token with the remote application
-    access_token_url='https://api.twitter.com/oauth/access_token',
-    # twitter knows two authorizatiom URLs.  /authorize and /authenticate.
-    # they mostly work the same, but for sign on /authenticate is
-    # expected because this will give the user a slightly different
-    # user interface on the twitter side.
-    authorize_url='https://api.twitter.com/oauth/authenticate',
-    # the consumer keys from the twitter application registry.
-    consumer_key='xBeXxg9lyElUgwZT6AZ0A',
-    consumer_secret='aawnSpNTOVuDCjx7HMh6uSXetjNN8zWLpZwCEU4LBrk'
-)
-
-
-
-
-
 @app.before_request
 def before_request():
     g.user = None
